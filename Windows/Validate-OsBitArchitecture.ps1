8e9ab275ba300003ae1cb69e2590d043edea4b41
JonathonAnderson <49241459+JonathonAnderson@users.noreply.github.com>
2019-05-02 15:21:29 -0500

    Create Validate-OsBitArchitecture.ps1

---
+++ Windows/Validate-OsBitArchitecture.ps1
@@ -0,0 +1,42 @@
+<#
+    .AUTHOR
+        Jonathon Anderson
+    .COPYRIGHT 
+        Jonathon Anderson
+    .SYNOPSIS
+    .PARAMETER
+    .EXAMPLE
+        Validate-OsBitArchitecture -Is 32
+        Validate-OsBitArchitecture -Is 64
+        
+    .TODO
+#>
+
+function Validate-OsBitArchitecture
+{
+    [CmdletBinding()]
+    Param 
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('32','64')]
+        [string]$Is
+
+    )
+
+    BEGIN 
+    {
+        $OSBitArchitecture = $(Get-CimInstance -ClassName Win32_OperatingSystem -Property OSArchitecture).OSArchitecture
+    }
+
+    PROCESS 
+    {
+        switch -Regex ($OSBitArchitecture)
+        {
+            "([^\d])*(32)([^\d])*" { return $($Is -eq '32') }
+            "([^\d])*(64)([^\d])*" { return $($Is -eq '64') }
+        }
+
+    }
+
+    END {}
+}
