0740e45068cbe63e2aa98ff1ea4c9fd5fe740c43
JonathonAnderson <49241459+JonathonAnderson@users.noreply.github.com>
2019-06-11 14:32:32 -0700

    Create Manage-ADComputers.ps1

---
+++ ActiveDirectory/Manage-ADComputers.ps1
@@ -0,0 +1,50 @@
+﻿<#
+    .AUTHOR
+        Jonathon Anderson
+    .COPYRIGHT 
+        Jonathon Anderson
+
+    .SYNOPSIS
+    .PARAMETER
+    .EXAMPLE
+    .TODO
+#>
+
+function Manage-ADComputers
+{
+    [CmdletBinding()]
+    Param 
+    (
+        [timespan]$StaleTimeSpan,
+        [timespan]$ExpiredTimeSpan,
+        [Microsoft.ActiveDirectory.Management.ADOrganizationalUnit]$StaleOU
+    )
+
+    BEGIN 
+    {
+        $StaleComputers   = Search-ADAccount -ComputersOnly -AccountInactive -TimeSpan $StaleTimeSpan
+        $ExpiredComputers = Search-ADAccount -ComputersOnly -AccountInactive -TimeSpan $ExpiredTimeSpan
+
+        $DisabledComputers = Search-ADAccount -ComputersOnly -AccountDisabled
+
+    }
+
+    PROCESS 
+    {
+        foreach ($Computer in $StaleComputers)
+        {
+            Disable-ADAccount -Identity $Computer
+            Move-ADObject -Identity $Computer -TargetPath $StaleOU
+        }
+        foreach ($Computer in $DisabledComputers)
+        {
+            if($Computer.Name -in $ExpiredComputers.Name)
+            {
+                Remove-ADComputer -Identity $Computer -Confirm:$false
+            }
+        }
+    }
+
+    END {}
+}
+
