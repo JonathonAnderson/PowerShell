047a7f43c486b1d0461427c749edf09665541128
JonathonAnderson <49241459+JonathonAnderson@users.noreply.github.com>
2019-07-10 09:46:31 -0700

    misc

---
+++ NotMine/Redistribute-SccmPackage.ps1
@@ -0,0 +1,8 @@
+﻿$SiteCode = ""
+$PackageID = ""
+     $distpoints = Get-WmiObject -Namespace "root\SMS\Site_$($SiteCode)" -Query "Select * From SMS_DistributionPoint WHERE PackageID='$PackageID'"
+        foreach ($dp in $distpoints)
+        {
+                $dp.RefreshNow = $true
+                $dp.Put()
+        }
