86991d9685d5c2b614e822ad9bc103f59f05f384
JonathonAnderson <49241459+JonathonAnderson@users.noreply.github.com>
2019-07-25 10:05:06 -0700

    begin test-dhcprequest

---
+++ Network/Test-DhcpRequest.ps1
@@ -0,0 +1,11 @@
+﻿Function Test-DhcpRequest
+{    
+    Param
+    (
+      [ValidatePattern("^(?i:[A-F|\d]{2}(?:[\W|_]?)){5}(?i:[A-F|\d]{2})$")]
+      [ValidateLength(12,17)]
+      [Parameter(Mandatory=$true)]
+      [String]$ClientMacAddress
+    )
+
+}
